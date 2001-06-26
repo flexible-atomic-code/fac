7e60083d64616b1a53d6b4d9d3f147a8304f96d8
(no author) <(no author)@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2001-06-26 15:16:44 +0000

    This commit was manufactured by cvs2svn to create tag 'fac-0_1_4'.
    git-svn-id: file:///home/fnevgeny/svn/fac/tags/fac-0_1_4@15 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/faclib/nucleus.c
+++ fac/faclib/nucleus.c
@@ -1,13 +1,51 @@
 #include "nucleus.h"
 
+#define N_ELEMENTS 109
+
 static NUCLEUS atom;
+static char _ename[N_ELEMENTS][3] = 
+{"h", "he", "li", "be", "b", "c", "n", "o", "f",
+ "ne", "na", "mg", "al", "si", "p", "s", "cl", "ar", 
+ "k", "ca", "sc", "ti", "v", "cr", "mn", "fe", "co", 
+ "ni", "cu", "zn", "ga", "ge", "as", "se", "br", "kr",
+ "rb", "sr", "y", "zr", "nb", "mo", "tc", "ru", "rh", 
+ "pd", "ag", "cd", "in", "sn", "sb", "te", "i", "xe",
+ "cs", "ba", "la", "ce", "pr", "nd", "pm", "sm", "eu", 
+ "gd", "tb", "dy", "ho", "er", "tm", "yb", "lu", "hf",
+ "ta", "w", "re", "os", "ir", "pt", "au", "hg", "tl",
+ "pb", "bi", "po", "at", "rn", "fr", "ra", "ac", "th",
+ "pa", "u", "np", "pu", "am", "cm", "bk", "cf", "es",
+ "fm", "md", "no", "lr", "rf", "db", "sg", "bh", "hs", "mt"};
+
+static double _emass[N_ELEMENTS] = 
+{1, 4, 7, 9, 11, 12, 14, 16, 19, 20, 23, 24, 27, 28, 31, 32, 35, 40, 39, 
+ 40, 45, 48, 51, 52, 55, 56, 58, 59, 64, 65, 70, 73, 75, 79, 80, 84, 85,
+ 88, 89, 91, 93, 96, 98, 101, 103, 106, 108, 112, 115, 119, 122, 128, 127,
+ 131, 133, 137, 139, 140, 141, 144, 147, 150, 152, 157, 159, 162, 165, 167,
+ 169, 173, 175, 178, 181, 184, 186, 190, 190, 195, 197, 200, 204, 207, 209, 
+ 210, 210, 222, 223, 226, 227, 232, 231, 238, 237, 242, 243, 247, 247, 249, 
+ 254, 253, 256, 254, 257, 257, 260, 263, 262, 265, 266};
+
 
 int SetAtom(char *s, double z, double mass) {
+  int i;
   if (s == NULL) return -1;
   strncpy(atom.symbol, s, 2); 
+  if (z <= 0 || mass <= 0) {
+    for (i = 0; i < N_ELEMENTS; i++) {
+      if (strncasecmp(_ename[i], s, 2) == 0) {
+	if (z <= 0) z = i+1;
+	if (mass <= 0) mass = _emass[i];
+	break;
+      }
+    }
+  }
+  if (z <= 0) return -1;
   atom.atomic_number = z;
-  if (mass <= 0.0) atom.mass = 2.0*z;
+  if (mass <= 0.0) mass = 2.0*z;
+  atom.mass = mass;
   atom.rn = 2.2677E-5 * pow(mass, 1.0/3);
+  return 0;
 }
 
 double GetAtomicNumber() {
