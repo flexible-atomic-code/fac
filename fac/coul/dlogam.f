bfbd29d2236dda4c97f12be6f238c4b8bbfd9116
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2001-07-30 02:17:38 +0000

    added multiprecision package support
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@47 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/coul/dlogam.f
+++ fac/coul/dlogam.f
@@ -1,10 +1,10 @@
       function dlogam(x)
       double precision x, dlogam
-      complex*16 cx, clogam 
+      complex*16 cx, r, clogam, logam
 
+      r = logam(2D-16)
       cx = dcmplx(x, 0.0D0)
-      cx = clogam(cx)
-      dlogam = dble(cx)
+      r = clogam(cx)
+      dlogam = dble(r)
       return
       end
-      
