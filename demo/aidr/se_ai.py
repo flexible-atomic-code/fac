c73d4bfd99d3f11fbe8b0d8dbbe0cd6bb670a2d6
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2001-09-23 19:39:16 +0000

    update the demo scripts
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@92 ad7935cb-1c1c-0410-8b58-bafadb17b910

---
+++ demo/aidr/se_ai.py
@@ -0,0 +1,24 @@
+""" calculate the autoionization rates for Ne-like Se.
+"""
+
+# import the modules
+import fac
+import config
+
+fac.SetAtom('Se')
+
+# configurations for the F-like ion
+config.closed('1s')
+config.closed('2s')
+config.config('2p5', group = 'n2')
+
+# configurations of doubly excited Ne-like ion
+config.config('2p4 3s2', '2p4 3s1 3p1', group = 'n33')
+
+fac.OptimizeRadial('n33')
+fac.Structure('n2')
+fac.Structure('n33')
+
+fac.LevelTable('se.lev')
+fac.AITable(['n33'], ['n2'], 'se.ai')
+
