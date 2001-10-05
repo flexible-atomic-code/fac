bf2c72687be8633d466aa4575f045fdb3b5b22a5
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2001-10-05 19:24:28 +0000

    added grid.c
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@118 ad7935cb-1c1c-0410-8b58-bafadb17b910

---
+++ faclib/grid.h
@@ -0,0 +1,22 @@
+#ifndef _GRID_H_
+#define _GRID_H_ 1
+
+#include "global.h"
+
+#define MAXNKL         50
+#define MAXKL          1000
+#define MAXNUSR        30
+#define MAXNE          15
+#define MAXNTE         6
+
+int AddPW(int *nkl0, double *kl, double *logkl, 
+	  int maxkl, int n, int step);
+int SetPWGrid(int *nkl0, double *kl, double *logkl, 
+	      int maxkl, int *ns, int *n, int *step);
+int SetTEGridDetail(double *te, double *logte, int n, double *x);
+int SetTEGrid(double *te, double *logte, int n, double emin, double emax);
+int SetEGridDetail(double *e, double *log_e, int n, double *xg);
+int SetEGrid(double *e, double *log_e, 
+	     int n, double emin, double emax, double eth);
+
+#endif
