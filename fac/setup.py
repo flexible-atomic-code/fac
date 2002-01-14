9e07f4664411f1b4de1c0fb5f9ae8566bfa351c3
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2002-01-14 23:19:02 +0000

    vast improvemet. The output scheme changed, crm module added.
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@152 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/setup.py
+++ fac/setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup, Extension
 import os
 
-# change the fortran runtime lib. appropriately.
+# set the fortran runtime lib. appropriately.
 # this only works for sun solaris.
 fortranlib = ["F77", "M77", "sunmath"]
 
 # for Linux (or Windows with Cygwin), you would use:
 # fortranlib = ["g2c"]
 
-libs = ["fac", "lapack", "blas", "coul",
+libs = ["fac", "quadpack", "lapack", "blas", "coul",
         "toms", "mpfun", "minpack", "m"] + fortranlib
 
-libdir = ["faclib", "lapack", "blas", "coul",
-          "toms", "mpfun", "minpack"]
+libdir = ["faclib", "coul", "toms", "mpfun", "minpack",
+          "quadpack", "lapack", "blas"]
 incdir = ["faclib"]
     
 os.system("make lib")
@@ -21,10 +21,15 @@ os.system("make doc")
 setup(name = "FAC",
       version = "0.6.8",
       package_dir = {'pfac': 'python'},
-      py_modules = ['pfac.const', 'pfac.config', 'pfac.dr'],
+      py_modules = ['pfac.const', 'pfac.config', 'pfac.atom'],
       ext_modules = [Extension("pfac.fac",
                                ["python/fac.c"],
                                include_dirs = incdir,
                                library_dirs = libdir,
-                               libraries = libs)])
-
+                               libraries = libs),
+                     Extension("pfac.crm",
+                               ["python/pcrm.c"],
+                               include_dirs = incdir,
+                               library_dirs = libdir,
+                               libraries = libs)
+                     ])
