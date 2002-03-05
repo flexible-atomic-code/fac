d7600db6cb329c3062ed14ed588623d3147007f3
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2002-03-05 02:47:38 +0000

    minor
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@212 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- demo/FeL/fe.py
+++ demo/FeL/fe.py
@@ -1,5 +1,6 @@
 from pfac.atom import *
 from pfac import fac
+import os
 
 # by uncommenting the following line and the one at the end of this file,
 # this script will be converted to the input file for the SFAC interface.
@@ -8,7 +9,8 @@ from pfac import fac
 asym = 'Fe'
 
 # generate atomic data for H-like to Na-like ions.
-nele = range(5, 7)
+os.system('mkdir data')
+nele = range(1, 2)
 atomic_data(nele, asym, iprint=1, dir='data/')
 
 #fac.CloseSFAC()
