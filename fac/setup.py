d39de846cf24654460cf94e3192cceb48999b439
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2002-03-11 02:41:16 +0000

    put make macros in setup.py
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@216 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/setup.py
+++ fac/setup.py
@@ -1,23 +1,38 @@
 from distutils.core import setup, Extension
 import os
+import sys
 
 # set the fortran runtime lib. appropriately.
 # this only works for sun solaris.
 # fortranlib = ["F77", "M77", "sunmath"]
-
 # for Linux (or Windows with Cygwin), you would use:
-fortranlib = ["g2c"]
+# fortranlib = ["g2c"]
+
+platform = sys.platform[:5]
+if (platform == 'linux' or
+    platform == 'cygwi'):
+      fortranlib = ["g2c"]
+      fflags = '-O -c -fPIC'
+      cflags = '-O -c -fPIC'
+elif (platform == 'sunos'):
+      fortranlib = ["F77", "M77", "sunmath"]
+      fflags = '-O -c -KPIC'
+      cflags = '-O -c -KPIC'
+
+flib = ' '.join(map(lambda a:'-l%s'%a, fortranlib))
+macros = '"FORTRANLIB=%s" '%flib
+macros = macros + '"FFLAGS=%s" '%fflags
+macros = macros + '"CFLAGS=%s" '%cflags
+print macros
+os.system("make lib %s"%macros)
+os.system("make doc")
 
 libs = ["fac", "quadpack", "lapack", "blas", "coul",
         "toms", "mpfun", "minpack", "ionis", "m"] + fortranlib
-
 libdir = ["faclib", "coul", "toms", "mpfun", "minpack",
           "quadpack", "lapack", "blas", "ionis"]
 incdir = ["faclib"]
     
-os.system("make lib")
-os.system("make doc")
-
 setup(name = "FAC",
       version = "0.7.1",
       package_dir = {'pfac': 'python'},
