641d33156729a93f5cfb29c67b8cc7c7080b61a1
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2002-05-31 18:28:28 +0000

    adding FAQ
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@244 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ else:
       incdir = ["faclib"]
     
       setup(name = "FAC",
-            version = "0.7.3",
+            version = "0.7.4",
             package_dir = {'pfac': 'python'},
             py_modules = ['pfac.const', 'pfac.config', 'pfac.table',
                           'pfac.atom', 'pfac.spm'],
