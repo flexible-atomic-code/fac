2ae7b502aa942e5ebfc504e44497e21406ceae7d
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2003-03-09 02:06:15 +0000

    minor
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@353 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/setup.py
+++ fac/setup.py
@@ -15,7 +15,7 @@ for s in flibs:
             extralink.append(s)
 
 setup(name = "FAC",
-      version = "0.8.8",
+      version = "0.8.9",
       package_dir = {'pfac': 'python'},
       py_modules = ['pfac.const', 'pfac.config', 'pfac.table',
                     'pfac.atom', 'pfac.spm'],
