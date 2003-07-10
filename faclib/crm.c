dc054f9bccb884e569fd7c57702be867b05324bb
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2003-07-10 18:23:06 +0000

    backward compatibility
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@415 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- faclib/crm.c
+++ faclib/crm.c
@@ -2,7 +2,7 @@
 #include "grid.h"
 #include "cf77.h"
 
-static char *rcsid="$Id: crm.c,v 1.54 2003/07/10 14:04:38 mfgu Exp $";
+static char *rcsid="$Id: crm.c,v 1.55 2003/07/10 18:23:06 mfgu Exp $";
 #if __GNUC__ == 2
 #define USE(var) static void * use_##var = (&use_##var, (void *) &var) 
 USE (rcsid);
@@ -3505,7 +3505,9 @@ int SetCERates(int inv) {
 	e = ion->energy[r.upper] - ion->energy[r.lower];
 	data[1] = r.bethe;
 	cs = r.strength;
-	x[0] = h.te0/(h.te0+r.born[1]);
+	if (fh.sversion >= 9 && fh.ssversion >= 9) {
+	  x[0] = h.te0/(h.te0+r.born[1]);
+	}
 	y[0] = r.born[0];
 	if (r.bethe <= 0) {
 	  for (j = 0; j < m; j++) {
@@ -3576,7 +3578,9 @@ int SetCERates(int inv) {
 	  e = ion0.energy[q] - ion0.energy[p];
 	  data[1] = r.bethe;	
 	  cs = r.strength;
-	  x[0] = h.te0/(h.te0 + r.born[1]);
+	  if (fh.sversion >= 9 && fh.ssversion >= 9) {
+	    x[0] = h.te0/(h.te0 + r.born[1]);
+	  }
 	  y[0] = r.born[0];
 	  if (r.bethe <= 0) {
 	    for (j = 0; j < m; j++) {
