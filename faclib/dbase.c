c2f783bf585057c87034e83db61ded0a4f9e6681
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2004-02-25 17:59:55 +0000

    minor
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk/fac@496 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- faclib/dbase.c
+++ faclib/dbase.c
@@ -1,7 +1,7 @@
 #include "dbase.h"
 #include "cf77.h"
 
-static char *rcsid="$Id: dbase.c,v 1.55 2004/01/17 19:37:49 mfgu Exp $";
+static char *rcsid="$Id: dbase.c,v 1.56 2004/02/25 17:59:55 mfgu Exp $";
 #if __GNUC__ == 2
 #define USE(var) static void * use_##var = (&use_##var, (void *) &var) 
 USE (rcsid);
@@ -2683,8 +2683,11 @@ int PrintRRTable(FILE *f1, FILE *f2, int v, int swp) {
 	    ee = h.usr_egrid[t];
 	    eph = ee + e;
 	  }
-	  phi = 2.0*PI*FINE_STRUCTURE_CONST*r.strength[t]*AREA_AU20;
+	  phi = FINE_STRUCTURE_CONST2*ee;	  
+	  phi = (1.0+phi)/(1.0+0.5*phi);
+	  phi *= 2.0*PI*FINE_STRUCTURE_CONST*r.strength[t]*AREA_AU20;
 	  rr = phi * pow(FINE_STRUCTURE_CONST*eph, 2) / (2.0*ee);
+	  rr /= 1.0+0.5*FINE_STRUCTURE_CONST2*ee;
 	  phi /= (mem_en_table[r.b].j + 1.0);
 	  rr /= (mem_en_table[r.f].j + 1.0);
 	  fprintf(f2, "%11.4E %11.4E %11.4E %11.4E\n",
@@ -2945,7 +2948,7 @@ int PrintCITable(FILE *f1, FILE *f2, int v, int swp) {
 	if (v) {
 	  a = h.usr_egrid[t];
 	  if (h.usr_egrid_type == 1) a += e;
-	  a *= 1.0 + FINE_STRUCTURE_CONST2*a;
+	  a *= 1.0 + 0.5*FINE_STRUCTURE_CONST2*a;
 	  a = AREA_AU20/(2.0*a*(mem_en_table[r.b].j + 1.0));
 	  a *= r.strength[t];
 	  fprintf(f2, "%11.4E %11.4E %11.4E\n",
