50e8a989ebcb5e22837d8850e1ae30079fc21846
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2004-06-30 18:11:15 +0000

    minor
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@545 ad7935cb-1c1c-0410-8b58-bafadb17b910

--- fac/python/atom.py
+++ fac/python/atom.py
@@ -238,7 +238,7 @@ class ATOM:
             if (self.nele > 5):
                 self.nterms = [3, 2, 2, 2]
             self.nexc_max = [5, 5, 5, 5]
-            self.nfrozen = [10, 8, 7, 6]
+            self.nfrozen = [10, 8, 12, 6]
             self.nexc_rec = [10, 4, 4, 4]
             self.nrec_max = [25, 16, 12, 8]
             self.rec_pw_max = [10, 9, 9, 5]
@@ -254,9 +254,9 @@ class ATOM:
             self.n_shells = 3
             self.nterms = [-1,-1,-1,-1,-1]
             self.nexc_max = [5, 5, 5, 5, 5]
-            self.nfrozen = [10, 7, 6, 6, 6]
+            self.nfrozen = [10, 7, 6, 10, 6]
             self.nexc_rec = [4, 4, 4, 4, 0]
-            self.nrec_max = [25, 10, 10, 8, 8]
+            self.nrec_max = [25, 10, 10, 10, 8]
             self.rec_pw_max = [10, 9, 6, 5, 5]
             self.nrec_ext = 45
             self.n_decay = [10, 4, 4, 4, 4]
