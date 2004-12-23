65d24a47a7414e9cb060a5c1d4aadc23d66d1587
mfgu <mfgu@ad7935cb-1c1c-0410-8b58-bafadb17b910>
2004-12-23 21:31:03 +0000

    rmatrix manual and demo
    git-svn-id: file:///home/fnevgeny/svn/fac/trunk@587 ad7935cb-1c1c-0410-8b58-bafadb17b910

---
+++ fac/demo/rmatrix/cs.py
@@ -0,0 +1,25 @@
+from pfac.fac import *
+import time
+
+"""
+Calculate the collision strengths using the R-matrix method.
+"""
+
+#ConvertToSFAC('trmx.sf')
+
+t0 = time.time()
+
+# list of R-matrix basis files, calculated with rmx.py
+b = ['rmx.b0', 'rmx.b1']
+
+# list of R-matrix surface files.
+r = ['rmx.d0', 'rmx.d1']
+
+# call RMatrixCE, calculate collsion strength at E=750--1.5E3 eV,
+# in 100 eV step. Results are in r1.d
+RMatrixCE('r1.d'%i, b, r, 750.0, 1.51e3, 100.0)
+
+t1 = time.time()
+print 'Time = %12.5E'%(t1-t0)
+
+#CloseSFAC()    
