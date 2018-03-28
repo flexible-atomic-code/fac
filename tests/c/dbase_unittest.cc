0269c594b8a82ac3f6fc7e157e7dca0e23a7b246
fujiisoup <fujiisoup@gmail.com>
2018-03-28 23:25:36 +0900

    remove unnecessary comments

--- tests/c/dbase_unittest.cc
+++ tests/c/dbase_unittest.cc
@@ -7,13 +7,8 @@ extern "C"{
 namespace {
 
 TEST(FindLevelByName, First) {
-  // This test is named "Negative", and belongs to the "FactorialTest"
-  // test case.
   char filename[] = "c/examples/li.lev.b";
   int nele = 3;
-  char nc[] = "1*2 2*1";
-  char cnr[] = "2s1";
-  char cr[] = "2s+1(1)1";
   int i;
 
   i = FindLevelByName(filename, nele, "1*2 2*1", "2s1", "2s+1(1)1");
