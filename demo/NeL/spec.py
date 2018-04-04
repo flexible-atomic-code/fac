f095caf2703713e41c705c3523521ff298addc0c
Keisuke Fujii <fUjiisoup@gmail.com>
2018-04-05 07:02:30 +0900

    Simpler CR model demo (#11)
    * Added a lithium CR model demo
    
    openmp parallization of rmatrix module
    
    remove duplicate omp private variable in rmatirx
    
    fix openmp segfault in structure
    
    Remove Li crmodel. Added Ne crm.
    
    * Allow more error in *.ai file.

---
+++ demo/NeL/spec.py
@@ -0,0 +1,50 @@
+"""
+s.py runs the collisional radiative module. sel.py prints the line intensities
+in ascii format.
+"""
+from pfac.crm import *
+from pfac import fac
+from pfac import spm
+
+# atomic number
+z = 10
+# number of electrons
+nele = 3
+
+temp = 500.0*((1+z-nele)/16.0)**2
+dens = 1.0
+
+# a is the atomic symbol corresponding to z
+a = fac.ATOMICSYMBOL[z]
+
+p = '%s%02d'%(a, nele)
+
+# add the ion with nele number of electrons
+AddIon(nele, 0.0, '%sb'%p)
+SetBlocks(-1)
+
+SetEleDist(0, temp, -1, -1)
+# radiative transition rates
+SetTRRates(0)
+# collisional excitation rate coefficients.
+SetCERates(1)
+
+# abundance of the ion with nele number of electrons
+SetAbund(nele, 1.0)
+SetEleDensity(dens)
+
+InitBlocks()
+
+SetIteration(1e-6, 0.5)
+
+LevelPopulation()
+
+# spectral data table
+SpecTable(p+'b.sp', 0)
+
+PrintTable(p+'b.sp', p+'a.sp')
+
+# save rate matrix
+rate = spm.get_complexes(nele)
+RateTable(p+'b.rt', rate)
+PrintTable(p+'b.rt', p+'a.rt', 1)
--- demo/check.py
+++ demo/check.py
@@ -33,7 +33,7 @@ def _get_header(lines):
 
 
 def read_lev(filename):
-    """ read *a.lev file. """
+    """ read *a.lev / *a.en file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -59,7 +59,7 @@ def read_lev(filename):
         block['sname'] = np.chararray(nlev, itemsize=20)
         block['name'] = np.chararray(nlev, itemsize=30)
         lines = lines[1:]
-        
+
         for i, line in enumerate(lines):
             if line.strip() == '':  # if empty
                 blocks = read_blocks(lines[i+1:])
@@ -75,12 +75,12 @@ def read_lev(filename):
             block['name'][i] = line[85:].strip()
 
         return (block, )
-            
+
     return header, read_blocks(lines)
 
 
 def read_tr(filename):
-    """ read *a.lev file. """
+    """ read *a.tr file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -105,7 +105,7 @@ def read_tr(filename):
         block['gf'] = np.zeros(ntrans, dtype=float)
         block['rate'] = np.zeros(ntrans, dtype=float)
         block['multipole'] = np.zeros(ntrans, dtype=float)
-        
+
         for i, line in enumerate(lines):
             if line.strip() == '':  # if empty
                 blocks = read_blocks(lines[i+1:])
@@ -118,14 +118,14 @@ def read_tr(filename):
             block['gf'][i] = float(line[35:48])
             block['rate'][i] = float(line[49:62])
             block['multipole'][i] = float(line[63:76])
-  
+
         return (block, )
 
     return header, read_blocks(lines)
 
 
 def read_ai(filename):
-    """ read *a.lev file. """
+    """ read *a.ai file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -148,8 +148,10 @@ def read_ai(filename):
         block['bound_2J'] = np.zeros(ntrans, dtype=int)
         block['free_index'] = np.zeros(ntrans, dtype=int)
         block['free_2J'] = np.zeros(ntrans, dtype=int)
-        block['rate'] = np.zeros((ntrans, negrid), dtype=float)
-        
+        block['Delta E'] = np.zeros(ntrans, dtype=float)
+        block['AI rate'] = np.zeros(ntrans, dtype=float)
+        block['DC strength'] = np.zeros(ntrans, dtype=float)
+
         for i, line in enumerate(lines):
             if line.strip() == '':  # if empty
                 blocks = read_blocks(lines[i+1:])
@@ -158,15 +160,17 @@ def read_ai(filename):
             block['bound_2J'][i] = int(line[8:10])
             block['free_index'][i] = int(line[14:17])
             block['free_2J'][i] = int(line[18:20])
-            block['rate'][i] = [float(l) for l in line[21:].split()]
-  
+            block['Delta E'][i] = float(line[21:32])
+            block['AI rate'][i] = float(line[33:44])
+            block['DC strength'][i] = float(line[45:56])
+
         return (block, )
 
     return header, read_blocks(lines)
-    
+
 
 def read_ce(filename):
-    """ read *a.en file. """
+    """ read *a.ce file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -186,7 +190,7 @@ def read_ce(filename):
         block['TEGRID'] = np.zeros(ntegrid, dtype=float)
         for i in range(ntegrid):
             block['TEGRID'][i] = float(lines.pop(0))
-    
+
         block['TE0'], lines = _read_value(lines, float)
         block['ETYPE'], lines = _read_value(lines, int)
         negrid, lines = _read_value(lines, int)
@@ -212,10 +216,10 @@ def read_ce(filename):
         else:
             block['collision strength'] = np.zeros((ntrans, nusr), dtype=float)
             block['crosssection'] = np.zeros((ntrans, nusr), dtype=float)
-        
+
         if block['MSUB']:
             block['ratio collision strength'] = np.zeros(ntrans, dtype=float)
-            
+
         nsub = np.zeros(ntrans, dtype=int)
         if block['QKMODE'] == 2:
             block['params'] == np.zeros((ntrans, 4), dtype=float)
@@ -245,7 +249,7 @@ def read_ce(filename):
                 if block['MSUB']:
                     line = lines[0]
                     lines = lines[1:]
-                    block['ratio collision strength'][tr] = float(line)                    
+                    block['ratio collision strength'][tr] = float(line)
                     for i in range(nusr):
                         line = lines[0]
                         lines = lines[1:]
@@ -255,14 +259,14 @@ def read_ce(filename):
                     if sub < nsub - 1:
                         line = lines[0]
                         lines = lines[1:]  # skip separator -----
-                    
+
                 else:
                     for i in range(nusr):
                         line = lines[0]
                         lines = lines[1:]
                         block['collision strength'][tr, i] = float(line[12:23])
-                        block['crosssection'][tr, i] = float(line[24:])                    
-                    
+                        block['crosssection'][tr, i] = float(line[24:])
+
         if len(lines) < 3:
             return (block, )
 
@@ -277,7 +281,7 @@ def read_ce(filename):
 
 
 def read_ci(filename):
-    """ read *a.lev file. """
+    """ read *a.ci file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -318,7 +322,7 @@ def read_ci(filename):
         block['parameters'] = np.zeros((ntrans, nparams), dtype=float)
         block['collision strength'] = np.zeros((ntrans, nusr), dtype=float)
         block['crosssection'] = np.zeros((ntrans, nusr), dtype=float)
-        
+
         for tr in range(ntrans):
             line = lines[0]
             lines = lines[1:]
@@ -335,7 +339,7 @@ def read_ci(filename):
                 lines = lines[1:]
                 block['collision strength'][tr, i] = float(line[12:23])
                 block['crosssection'][tr, i] = float(line[24:])
-              
+
             if len(lines) < 3:
                 return (block, )
 
@@ -350,7 +354,7 @@ def read_ci(filename):
 
 
 def read_rr(filename):
-    """ read *a.lev file. """
+    """ read *a.rr file. """
     with open(filename, 'r') as f:
         lines = f.readlines()
 
@@ -392,7 +396,7 @@ def read_rr(filename):
         block['RR crosssection'] = np.zeros((ntrans, nusr), dtype=float)
         block['PI crosssection'] = np.zeros((ntrans, nusr), dtype=float)
         block['gf'] = np.zeros((ntrans, nusr), dtype=float)
-        
+
         for tr in range(ntrans):
             line = lines[0]
             lines = lines[1:]
@@ -410,7 +414,7 @@ def read_rr(filename):
                 block['RR crosssection'][tr, i] = float(line[12:23])
                 block['PI crosssection'][tr, i] = float(line[24:35])
                 block['gf'][tr, i] = float(line[36:])
-              
+
             if len(lines) < 3:
                 return (block, )
 
@@ -424,12 +428,125 @@ def read_rr(filename):
     return header, read_blocks(lines)
 
 
+def read_sp(filename):
+    """ read *a.sp file. """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    # header
+    header, lines = _get_header(lines)
+    header['Nblocks'] = _read_value(lines, int)
+    lines = lines[1:]
+
+    def read_blocks(lines):
+        block = {}
+        block['NELE'], lines = _read_value(lines, int)
+        ntrans, lines = _read_value(lines, int)
+        block['TYPE'], lines = _read_value(lines, str)
+        block['IBLK'], lines = _read_value(lines, int)
+        block['ICOMP'], lines = _read_value(lines, str)
+        block['FBLK'], lines = _read_value(lines, int)
+        block['FCOMP'], lines = _read_value(lines, str)
+
+        # read the values
+        block['block'] = np.zeros(ntrans, dtype=int)
+        block['level'] = np.zeros(ntrans, dtype=int)
+        block['abs. energy'] = np.zeros(ntrans, dtype=float)
+        block['population'] = np.zeros(ntrans, dtype=float)
+        block['Delta E'] = np.zeros(ntrans, dtype=float)
+        block['emissivity'] = np.zeros(ntrans, dtype=float)
+
+        for tr in range(ntrans):
+            line = lines[0]
+            lines = lines[1:]
+
+            if line.strip() == '':  # if empty
+                blocks = read_blocks(lines[i+1:])
+                return (block, ) + blocks
+
+            block['block'][tr] = int(line[:7])
+            block['level'][tr] = int(line[8:14])
+            block['abs. energy'][tr] = float(line[15:28])
+            block['population'][tr] = float(line[29:40])
+            block['Delta E'][tr] = float(line[15:28])
+            block['emissivity'][tr] = float(line[29:40])
+
+        return (block, )
+
+    return header, read_blocks(lines)
+
+
+def read_rt(filename):
+    """ read *a.rt file. """
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+
+    # header
+    header, lines = _get_header(lines)
+    header['Nblocks'] = _read_value(lines, int)
+    lines = lines[1:]
+
+    def read_blocks(lines):
+        block = {}
+        ntrans, lines = _read_value(lines, int)
+        block['EDEN'], lines = _read_value(lines, float)
+        block['EDIST'], lines = _read_value(lines, int)
+        npedis, lines = _read_value(lines, int)
+        block['EDIS'] = np.zeros(npedis, float)
+        for i in range(npedis):
+            line = lines[0]
+            lines = lines[1:]
+            block['EDIS'][i] = float(line)
+        block['PDEN'], lines = _read_value(lines, float)
+        block['PDIST'], lines = _read_value(lines, int)
+        nppdis, lines = _read_value(lines, int)
+        block['PPDIS'] = np.zeros(nppdis, float)
+        for i in range(npedis):
+            line = lines[0]
+            lines = lines[1:]
+            block['PPDIS'][i] = float(line)
+        lines = lines[1:] # skip header
+
+        # read the values
+        block['block'] = np.zeros(ntrans, dtype=int)
+        block['level'] = np.zeros(ntrans, dtype=int)
+        block['NB'] = np.zeros(ntrans, dtype=int)
+        block['TR'] = np.zeros(ntrans, dtype=int)
+        block['CE'] = np.zeros(ntrans, dtype=int)
+        block['RR'] = np.zeros(ntrans, dtype=int)
+        block['AI'] = np.zeros(ntrans, dtype=int)
+        block['CI'] = np.zeros(ntrans, dtype=int)
+        block['ncomplex'] = np.chararray(ntrans, itemsize=20)
+
+        for tr in range(ntrans):
+            line = lines[0]
+            lines = lines[1:]
+
+            if line.strip() == '':  # if empty
+                blocks = read_blocks(lines[i+1:])
+                return (block, ) + blocks
+
+            block['block'][tr] = int(line[:7])
+            block['level'][tr] = int(line[8:12])
+            block['NB'][tr] = float(line[13:25])
+            block['TR'][tr] = float(line[26:37])
+            block['CE'][tr] = float(line[38:49])
+            block['RR'][tr] = float(line[50:61])
+            block['AI'][tr] = float(line[62:73])
+            block['CI'][tr] = float(line[74:85])
+            block['ncomplex'][tr] = line[86:].strip()
+
+        return (block, )
+
+    return header, read_blocks(lines)
+
+
 def _check_header(actual, expected):
     if 'E0' in actual:
         assert np.allclose(actual['E0'], expected['E0'], rtol=0.01)
     if 'NBlocks' in actual:
         assert actual['NBlocks'] == expected['NBlocks']
-    
+
 
 def _check_block(actual_blocks, expected_blocks, atols={}, rtols={}):
     """ Compare each values in two blocks.
@@ -443,7 +560,7 @@ def _check_block(actual_blocks, expected_blocks, atols={}, rtols={}):
             ind_a = np.unravel_index(np.argmax(np.abs(actual - expected)),
                                      actual.shape)
             ind_r = np.unravel_index(
-                np.argmax(0.5 * np.abs(actual - expected) / 
+                np.argmax(0.5 * np.abs(actual - expected) /
                     (np.abs(actual) + np.abs(expected))), actual.shape)
             message += ' The largest abs difference is in {}'.format(ind_a)
             message += ' with {} vs {} '.format(actual[ind_a], expected[ind_a])
@@ -452,7 +569,7 @@ def _check_block(actual_blocks, expected_blocks, atols={}, rtols={}):
         else:
             message += ' {} vs {}'.format(actual, expected)
         raise ValueError(message)
-                
+
     for i, (actual_bl, expected_bl) in  enumerate(
             zip(actual_blocks, expected_blocks)):
         for key in actual_bl:
@@ -476,7 +593,7 @@ def _check_block(actual_blocks, expected_blocks, atols={}, rtols={}):
                                        atol=atols.get(key, 1.0e-8),
                                        rtol=rtols.get(key, 1.0e-5)):
                         _raise(i, key, actual, expected)
-                    
+
             elif isinstance(actual, float):
                 if not np.allclose(actual_bl[key], expected_bl[key],
                                atol=atols.get(key, 1.0e-8),
@@ -490,9 +607,9 @@ def _check_block(actual_blocks, expected_blocks, atols={}, rtols={}):
 def check_en(actual_file, expected_file):
     actual_header, actual_blocks = read_lev(actual_file)
     expected_header, expected_blocks = read_lev(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
+    _check_block(actual_blocks, expected_blocks,
                  atols={'ENERGY': 1.0e1},
                  rtols={'ENERGY': 0.01})
 
@@ -500,70 +617,89 @@ def check_en(actual_file, expected_file):
 def check_ai(actual_file, expected_file):
     actual_header, actual_blocks = read_ai(actual_file)
     expected_header, expected_blocks = read_ai(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
+    _check_block(actual_blocks, expected_blocks,
                  atols={'rate': 1.0e1},
-                 rtols={'rate': 0.01})
-    
+                 rtols={'rate': 0.01, 'DC strength': 0.01,
+                        'AI rate': 0.01})
+
 
 def check_tr(actual_file, expected_file):
     actual_header, actual_blocks = read_tr(actual_file)
     expected_header, expected_blocks = read_tr(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
-                 atols={'Delta E': 1.0e0, 'gf': 1.0e-4, 'rate': 1.0e4, 
+    _check_block(actual_blocks, expected_blocks,
+                 atols={'Delta E': 1.0e0, 'gf': 1.0e-4, 'rate': 1.0e4,
                         'multipole': 1.0e-5},
-                 rtols={'Delta E': 0.01, 'gf': 0.02, 'rate': 0.05, 
+                 rtols={'Delta E': 0.01, 'gf': 0.02, 'rate': 0.05,
                         'multipole': 0.01})
-    
+
 
 def check_ce(actual_file, expected_file):
     actual_header, actual_blocks = read_ce(actual_file)
     expected_header, expected_blocks = read_ce(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
-                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0, 
+    _check_block(actual_blocks, expected_blocks,
+                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0,
                         'collision strength': 0.001, 'crosssection': 0.01,
-                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0, 
+                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0,
                         'TE0': 1.0},
-                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01, 
+                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01,
                         'collision strength': 0.02, 'crosssection': 0.1,
-                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01, 
+                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01,
                         'TE0': 0.01})
 
 
 def check_ci(actual_file, expected_file):
     actual_header, actual_blocks = read_ci(actual_file)
     expected_header, expected_blocks = read_ci(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
-                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0, 
+    _check_block(actual_blocks, expected_blocks,
+                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0,
                         'collision strength': 0.001, 'crosssection': 0.01,
-                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0, 
+                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0,
                         'TE0': 1.0},
-                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01, 
+                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01,
                         'collision strength': 0.02, 'crosssection': 0.1,
-                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01, 
+                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01,
                         'TE0': 0.01, 'parameters': 0.01})
 
 
 def check_rr(actual_file, expected_file):
     actual_header, actual_blocks = read_rr(actual_file)
     expected_header, expected_blocks = read_rr(expected_file)
-    
+
     _check_header(actual_header, expected_header)
-    _check_block(actual_blocks, expected_blocks, 
-                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0, 
-                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0, 
+    _check_block(actual_blocks, expected_blocks,
+                 atols={'Delta E': 1.0e0, 'bethe': 1.0e0, 'born': 1.0,
+                        'TEGRID': 1.0, 'EGRID': 1.0, 'USR': 1.0,
                         'TE0': 1.0},
-                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01, 
+                 rtols={'Delta E': 0.01, 'bethe': 0.01, 'born': 0.01,
                         'RR crosssection': 0.01, 'PI crosssection': 0.01,
-                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01, 
+                        'TEGRID': 0.01, 'EGRID': 0.01, 'USR': 0.01,
                         'TE0': 0.01, 'parameters': 0.01, 'gf': 0.01})
+
+
+def check_sp(actual_file, expected_file):
+    actual_header, actual_blocks = read_sp(actual_file)
+    expected_header, expected_blocks = read_sp(expected_file)
+
+    _check_header(actual_header, expected_header)
+    _check_block(actual_blocks, expected_blocks,
+                 atols={'Delta E': 1.0e0, 'emissivity': 1.0e-10},
+                 rtols={'Delta E': 0.01, 'emissivity': 0.01})
+
+
+def check_rt(actual_file, expected_file):
+    actual_header, actual_blocks = read_rt(actual_file)
+    expected_header, expected_blocks = read_rt(expected_file)
+
+    _check_header(actual_header, expected_header)
+    _check_block(actual_blocks, expected_blocks)
 
 
 def check(actual_file, expected_file):
@@ -572,19 +708,25 @@ def check(actual_file, expected_file):
 
     elif actual_file[-3:] == '.ai':
         check_ai(actual_file, expected_file)
-    
+
     elif actual_file[-3:] == '.tr':
         check_tr(actual_file, expected_file)
-    
+
     elif actual_file[-3:] in ['.ce', 'ceM']:
         check_ce(actual_file, expected_file)
-    
+
     elif actual_file[-3:] == '.ci':
         check_ci(actual_file, expected_file)
-    
+
     elif actual_file[-3:] == '.rr':
         check_rr(actual_file, expected_file)
-    
+
+    elif actual_file[-3:] == '.sp':
+        check_sp(actual_file, expected_file)
+
+    elif actual_file[-3:] == '.rt':
+        check_rt(actual_file, expected_file)
+
     else:
         raise TypeError('Unknown file extension: {}'.format(actual_file))
 
