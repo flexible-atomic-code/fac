80812b3f262eb45b9a18c51b3eeb63cc95a321eb
Keisuke Fujii <fUjiisoup@gmail.com>
2018-04-07 23:29:16 +0900

    Openmp test (#19)
    * Test with openmp
    
    Add requirement file for openmp test
    
    Add a missing colon.
    
    import sys
    
    Remove too verbose tests
    
    * Update make check in order to take the random ordering within each block into account.

--- demo/excitation/fe17_excitation.py
+++ demo/excitation/fe17_excitation.py
@@ -1,8 +1,16 @@
 """calculate the electron impact excitation cross sections
 """
-
-# import the modules
+import sys
 from pfac import fac
+
+
+use_openmp = False
+if len(sys.argv) == 2 and sys.argv[1] == 'openmp':
+    use_openmp = True
+
+if use_openmp:
+    # enable openmp with 2 cores
+    fac.InitializeMPI(2)
 
 fac.SetAtom('Fe')
 # 1s shell is closed
@@ -21,3 +29,5 @@ fac.PrintTable('ne.lev.b', 'ne.lev', 1)
 fac.CETable('ne.ce.b', ['n2'], ['n3'])
 fac.PrintTable('ne.ce.b', 'ne.ce', 1)
 
+if use_openmp:
+    fac.FinalizeMPI()
--- demo/ionization/fe17_ionization.py
+++ demo/ionization/fe17_ionization.py
@@ -1,8 +1,16 @@
 """ calculate the electron impact ionization cross sections
 """
-
-# import the modules
+import sys
 from pfac import fac
+
+use_openmp = False
+if len(sys.argv) == 2 and sys.argv[1] == 'openmp':
+    use_openmp = True
+
+
+if use_openmp:
+    # enable openmp with 2 cores
+    fac.InitializeMPI(2)
 
 fac.SetAtom('Fe')
 # 1s shell is closed
@@ -26,3 +34,6 @@ e = [500.0, 900.0, 1.3e3, 1.7e3, 2.1e3, 4.2e3, 6.0e3, 8.0e3]
 fac.SetUsrCIEGrid(e)
 fac.CITable('ne.ci.b', ['fe17'], ['fe18'])
 fac.PrintTable('ne.ci.b', 'ne.ci', 1)
+
+if use_openmp:
+    fac.FinalizeMPI()
