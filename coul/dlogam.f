c8876373d0973f275abb9bd18c9915cee66142dc
Ming F Gu <mingfenggu2003@gmail.com>
2020-02-03 12:01:00 +0100

    fix omp issue with coulcc

--- coul/dlogam.f
+++ coul/dlogam.f
@@ -2,7 +2,7 @@
       double precision x, dlogam
       complex*16 cx, r, clogam, logam
 
-      r = logam(2D-16)
+c      r = logam(2D-16)
       cx = dcmplx(x, 0.0D0)
       r = clogam(cx)
       dlogam = dble(r)
